524916c9acfef10cb7f3469b4d6935c6efa68f54
Dave Tucker <dave@dtucker.co.uk>
2017-03-23 15:39:48 +0000

    WIP
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- src/hostnet/hostnet_http.ml
+++ src/hostnet/hostnet_http.ml
@@ -32,12 +32,13 @@ module Make
     (Tcp:V1_LWT.TCPV4)
     (Socket: Sig.SOCKETS)
     (Time: V1_LWT.TIME)
-    (Clock: V1.CLOCK) = struct
+    (Clock: V1.CLOCK)
+    = struct
 
   module Ch = Channel.Make(Tcp)
   module IO = Cohttp_mirage_io.Make(Ch)
-  module Request = Cohttp.Request.Make(IO)
-  module Response = Cohttp.Response.Make(IO)
+  module Req_io = Cohttp.Request.Make(IO)
+  module Res_io = Cohttp.Response.Make(IO)
 
   type t = {
     config: Config.t;
@@ -53,19 +54,45 @@ module Make
     let listeners port =
       Log.debug (fun f -> f "TCP handshake complete");
       Some (fun flow ->
-        let c = Ch.create flow in
-        Request.read c
-        >>= function
-        | `Eof ->
-          Log.info (fun f -> f "EOF on HTTP");
-          Tcp.close flow
-        | `Invalid x ->
-          Log.info (fun f -> f "Invalid HTTP request: %s" x);
-          Tcp.close flow
-        | `Ok request ->
-          Log.info (fun f -> f "HTTP %s" (Uri.to_string @@ Cohttp.Request.uri request));
-          Tcp.close flow
-      ) in
+          let open Cohttp in
+          let c = Ch.create flow in
+          Req_io.read c
+          >>= function
+          | `Eof ->
+            Log.info (fun f -> f "EOF on HTTP");
+            Tcp.close flow
+          | `Invalid x ->
+            Log.info (fun f -> f "Invalid HTTP request: %s" x);
+            Tcp.close flow
+          | `Ok request ->
+            let uri = Request.uri request in
+            Log.info (fun f -> f "HTTP %s" (Uri.to_string @@ uri));
+            (* Strip out hop-by-hop connection headers *)
+            let headers =
+              Request.headers request |> fun h ->
+              Header.remove h "accept-encoding" |> fun h ->
+              Header.remove h "content-length" |> fun h ->
+              Header.remove h "transfer-encoding" |> fun h ->
+              Header.remove h "connection" |> fun h ->
+              Header.add h "accept-encoding" "identity"
+            in
+            (* Fetch the remote URI *)
+            let meth = Request.meth request in
+            let reader = Req_io.make_body_reader request c in
+            let stream = Cohttp_lwt_body.create_stream Req_io.read_body_chunk reader in
+            let body = Cohttp_lwt_body.of_stream stream in
+            Log.info (fun f -> f "Trying to talk to %s" (Uri.to_string @@ uri));
+            let ctx = Cohttp_mirage.Client.default_ctx in
+            Cohttp_mirage.Client.call ~ctx ~headers ~body meth uri >>= fun (resp, body) ->
+            let status = Response.status resp in
+            let headers =
+              Response.headers resp |> fun h ->
+              Header.remove h "transfer-encoding" |> fun h ->
+              Header.remove h "content-length" |> fun h ->
+              Header.remove h "connection"
+            in
+            Tcp.close flow
+        ) in
     Lwt.return listeners
 
 end
